8c5c23fc1b487571bdb9f46da082ea351093fabc
Soe Thant Min <soemin@c1r6s5.42singapore.sg>
2025-09-29 16:08:54 +0800

    updated main.c with while loop, need a parsing function to run

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: soemin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/03 13:48:34 by soemin            #+#    #+#             */
-/*   Updated: 2025/09/29 15:52:29 by soemin           ###   ########.fr       */
+/*   Updated: 2025/09/29 16:06:20 by soemin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,23 @@ int	main(int ac, char **av, char **envp)
 	(void)av;
 	(void)envp;
 	last_status = 0;
-	ft_putstr_fd("Hello", 1);
+	while (1)
+	{
+		line = readline("minishell$ ");
+		if (!line)
+		{
+			printf("exit\n");
+			break;
+		}
+		if (*line)
+			add_history(line);
+		args = // parsing function
+		if (is_builtin(args))
+			run_builtin(args, envp, var_name, last_status);
+		else
+			run_binary(args);
+		free(args);
+		free(line);
+	}
+	return (0);
 }
